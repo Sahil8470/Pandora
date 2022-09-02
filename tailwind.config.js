3e0f60f77959811e3a43ed55cd8606279873d84e
Sahil8470 <gupta2003.sahil@gmail.com>
2022-09-02 23:07:31 +0530

    Header part completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.{html,js}"],
+    content: ["./*.html"],
     theme: {
+        screens: {
+            sm: "480px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px",
+        },
         colors: {
-            White: "#ffffff",
+            white: "#ffffff",
+        },
+        fontSize: {
+            "8xl": "6rem",
         },
         extend: {
             backgroundImage: {
